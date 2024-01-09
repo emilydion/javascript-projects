afc20501390f984b986fd1ad559ccb18fb60b757
emilydion <edion468@gmail.com>
2024-01-09 17:43:23 -0600

    Adding errors and debugging exercises

--- errors-and-debugging/exercises/DebuggingLogicErrors1.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors1.js
@@ -3,6 +3,8 @@
 // Did it?
 // Do not worry about fixing the code yet, we will do that in the next series of exercises.
 
+//It did launch, but should not have. The fuel level is too low. However, due to the order of the if/else statements, launchReady is incorrectly set to "true" and starts incorrectly. 
+
 let launchReady = false;
 let fuelLevel = 17000;
 let crewStatus = true;
