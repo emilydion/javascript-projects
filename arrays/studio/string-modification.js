afc7970782cc178da5c4885eaebb23e86396a2b8
emilydion <edion468@gmail.com>
2024-01-18 21:06:56 -0600

    Adding arrays studio

--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -9,3 +9,14 @@ let str = "LaunchCode";
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
 
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+let pigLatin = ""; 
+let length = input.question("How many letters would you like to be removed? "); 
+
+if (length > str.length) {
+    length = 3; 
+    console.log(`The value entered was longer than the word, so it has been defaulted to 3.`); 
+}
+
+pigLatin = str.slice(length, str.length) + str.slice(0, length);
+
+console.log(`${str}, in this pseudo-pig latin, becomes \"${pigLatin}\"`); 
