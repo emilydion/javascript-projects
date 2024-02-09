f60a34a4f81f593186af372927d95d396e1682c6
emilydion <edion468@gmail.com>
2024-02-08 20:26:51 -0600

    Adding Class 9 Studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,24 @@
 
 let launchcode = {
-  
-}
+  organization: "nonprofit",
+  executiveDirector: "Jeff",
+  percentageCoolEmployees: 100,
+  programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+  launchOutput: function(number) {
+    let finalString = "";
+    if (number % 2 === 0) {finalString += "Launch";}
+    if (number % 3 === 0) {finalString += "Code";}
+    if (number % 5 === 0 && (number % 2 === 0 || number % 3 === 0)) {finalString += " Rocks";}
+    else if (number % 5 === 0) {finalString += "Rocks";} 
+    finalString += "!"; 
+
+    if (number % 5 === 0 && number % 2 === 0 && number  % 3 !== 0) {finalString += " (CRASH!!!!)";}
+
+    if (number % 2 !== 0 && number % 3 !== 0 && number % 5 !== 0) {finalString = "Rutabagas! That doesn't work.";}
+
+    return finalString; 
+  }
+};
 
 module.exports = launchcode;
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -1,8 +1,55 @@
 // launchcode.test.js code:
 const launchcode = require('../index.js');
 
-describe("Testing launchcode", function(){
+describe("Testing launchcode", () => {
+  test("organization should equal \"nonprofit\"", () => {
+    expect(launchcode.organization).toEqual("nonprofit");
+  });
 
-  // Write your unit tests here!
-  
+  test("executiveDirector should equal \"Jeff\"", () => {
+    expect(launchcode.executiveDirector).toEqual("Jeff");
+  });
+
+  test("percentageCoolEmployees should equal \"Jeff\"", () => {
+    expect(launchcode.percentageCoolEmployees).toEqual(100);
+  });
+
+  test("programsOffered array should include \"Web Development\", \"Data Analysis\", and \"Liftoff\", and should be a length of 3.", () => {
+    expect(launchcode.programsOffered.includes("Web Development")).toBe(true);
+    expect(launchcode.programsOffered.includes("Data Analysis")).toBe(true);
+    expect(launchcode.programsOffered.includes("Liftoff")).toBe(true);
+    expect(launchcode.programsOffered.length).toEqual(3); 
+  });
+
+  test("launchOutput() should return \"Launch!\" when the value is ONLY divisible by 2.", () => {
+    expect(launchcode.launchOutput(8)).toEqual("Launch!"); 
+  });
+
+  test("launchOutput() should return \"Code!\" when the value is ONLY divisible by 3.", () => {
+    expect(launchcode.launchOutput(9)).toEqual("Code!"); 
+  });
+
+  test("launchOutput() should return \"Rocks!\" when the value is ONLY divisible by 5.", () => {
+    expect(launchcode.launchOutput(5)).toEqual("Rocks!"); 
+  });
+
+  test("launchOutput() should return \"LaunchCode!\" when the value is divisible by both 2 AND 3.", () => {
+    expect(launchcode.launchOutput(6)).toEqual("LaunchCode!"); 
+  });
+
+  test("launchOutput() should return \"Code Rocks!\" when the value is divisible by both 3 AND 5.", () => {
+    expect(launchcode.launchOutput(15)).toEqual("Code Rocks!"); 
+  });
+
+  test("launchOutput() should return \"Launch Rocks! (CRASH!!!!)\" when the value is divisible by both 2 AND 5.", () => {
+    expect(launchcode.launchOutput(10)).toEqual("Launch Rocks! (CRASH!!!!)"); 
+  });
+
+  test("launchOutput() should return \"LaunchCode Rocks!\" when the value is divisible by 2, 3, AND 5.", () => {
+    expect(launchcode.launchOutput(30)).toEqual("LaunchCode Rocks!"); 
+  });
+
+  test("launchOutput() should return \"Rutabagas! That doesn\'t work.\" when the value is divisible by 2, 3, AND 5.", () => {
+    expect(launchcode.launchOutput(17)).toEqual("Rutabagas! That doesn\'t work."); 
+  });
 });
