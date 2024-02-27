25b5c175a350b6206575ba311a6801aa1b604763
emilydion <edion468@gmail.com>
2024-02-26 20:32:32 -0600

    adding dom studio

--- dom-and-events/studio/scripts.js
+++ dom-and-events/studio/scripts.js
@@ -1,2 +1,45 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener("load", function() {
+    let takeoff = document.getElementById("takeoff"); 
+    let flightStatus = document.getElementById("flightStatus"); 
+    let shuttleBackground = document.getElementById("shuttleBackground"); 
+    let spaceShuttleHeight = document.getElementById("spaceShuttleHeight"); 
+    let landing = document.getElementById("landing"); 
+    let missionAbort = document.getElementById("missionAbort"); 
+    let up = document.getElementById("up"); 
+    let down = document.getElementById("down"); 
+    let left = document.getElementById("left"); 
+    let right = document.getElementById("right"); 
+    let rocket = document.getElementById("rocket"); 
+
+    takeoff.addEventListener("click", event => {
+        let takeoffConfirm = window.confirm("Confirm that the shuttle is ready for takeoff."); 
+        if (takeoffConfirm) {
+            flightStatus.innerHTML = "Shuttle in flight."
+            shuttleBackground.style.backgroundColor = "blue"; 
+            spaceShuttleHeight.innerHTML = parseInt(spaceShuttleHeight.innerHTML) + 10000;
+        }
+    });
+
+    landing.addEventListener("click", event => {
+        window.alert("The shuttle is landing. Landing gear engaged."); 
+        flightStatus.innerHTML = "The shuttle has landed."
+        shuttleBackground.style.backgroundColor = "green"; 
+        spaceShuttleHeight.innerHTML = 0; 
+    });
+
+    missionAbort.addEventListener("click", event => {
+        let missionAbortConfirm = window.confirm("Confirm that you want to abort the mission."); 
+        if (missionAbortConfirm) {
+            flightStatus.innerHTML = "Mission aborted."
+            shuttleBackground.style.backgroundColor = "green"; 
+            spaceShuttleHeight.innerHTML = 0; 
+        }
+    });
+
+    up.addEventListener("click", event => {
+        spaceShuttleHeight.innerHTML = parseInt(spaceShuttleHeight.innerHTML) + 10000;
+        rocket.style.top = "10px"; 
+    }); 
+});
